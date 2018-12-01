b035dbc1089597514e4c78c2ae4377e0c0bb107e
Azulinho <mail@azulinho.com>
2018-12-01 04:10:09 +0000

    boom

--- 0008.sh
+++ 0008.sh
@@ -1,2 +1,2 @@
-vdo create --name=vdoasync --device=/dev/md/0 \
-	   --vdoLogicalSize=1000G --writePolicy=async
+vdo create --name=vdoasync --device=/dev/vdo-rootfs \
+	   --vdoLogicalSize=10000G --writePolicy=async
